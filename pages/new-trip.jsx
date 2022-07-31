cbea3b36b1749c3e548ddf361e7d7c09a2a47f89
Harshvardhan Singh <hvsreal.223@gmail.com>
2022-07-31 10:22:57 +0530

    added few fields

--- pages/new-trip.jsx
+++ pages/new-trip.jsx
@@ -31,15 +31,44 @@ const Newtrip = () => {
   const { dispatch, state } = useContext(DataContext);
   const [payload, setPayload] = useState(ObjOfPayload);
   const [states, setStates] = useState([]);
-  const [cities, setCities] = useState([]);
+  const [FromCities, setFromCities] = useState([]);
+  const [ToCities, setToCities] = useState([]);
   const [errors, setErrors] = useState({
     Phone: "Phone Must Be A 10 Digit Number",
     Name: "Name is Missing",
+    "From State": "Please Select State",
   });
-  const handleChange = (e) => {
+  const handleNormChange = (e) => {
     setPayload({ ...payload, [e.target.name]: e.target.value });
     delete errors[e.target.name];
     setErrors(errors);
+  };
+  const handleDeepChange = (value, Key, Type) => {
+    payload[Type][Key] = value;
+    setPayload(payload);
+    delete errors[`${Type} ${Key}`];
+    setErrors(errors);
+  };
+  const handleOneSelect = (value, Key, Type) => {
+    payload[Type][Key] = value;
+    delete errors[`${Type} ${Key}`];
+    setErrors(errors);
+    if (Key == "State") {
+      payload[Type]["City"] = "";
+      if (Type == "From")
+        setFromCities(
+          City.getCitiesOfState("IN", value).map((each) => {
+            return { label: each.name, value: each.name };
+          })
+        );
+      else
+        setToCities(
+          City.getCitiesOfState("IN", value).map((each) => {
+            return { label: each.name, value: each.name };
+          })
+        );
+    }
+    setPayload(payload);
   };
 
   useEffect(() => {
@@ -57,7 +86,7 @@ const Newtrip = () => {
           <Input
             name="Name"
             value={payload.Name}
-            onChange={handleChange}
+            onChange={handleNormChange}
             type="text"
             placeholder="Your Name"
           />
@@ -68,17 +97,51 @@ const Newtrip = () => {
           <Input
             name="Phone"
             value={payload.Phone}
-            onChange={handleChange}
+            onChange={handleNormChange}
             type="number"
             placeholder="Your Phone"
           />
           <FormErrorMessage>{errors["Phone"]}</FormErrorMessage>
         </FormControl>
-        <FormControl w="20rem">
+        <FormControl w="20rem" isInvalid={errors["From State"]}>
           <Select
             placeholder="From State"
-            name="From.State"
-            onChange={handleSwitch}
+            onChange={(e) => handleOneSelect(e.target.value, "State", "From")}
+          >
+            {states.map((each, idx) => (
+              <option value={each.value} key={idx}>
+                {each.label}
+              </option>
+            ))}
+          </Select>
+          <FormErrorMessage>{errors["From State"]}</FormErrorMessage>
+        </FormControl>
+        <FormControl w="20rem">
+          <Select
+            placeholder="From City"
+            onChange={(e) => handleOneSelect(e.target.value, "City", "From")}
+          >
+            {FromCities.map((each, idx) => (
+              <option value={each.value} key={idx}>
+                {each.label}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl isInvalid={errors["From Place"]} w="20rem">
+          <Input
+            name="From Place"
+            value={payload.From.Place}
+            onChange={(e) => handleDeepChange(e.target.value, "Place", "From")}
+            type="text"
+            placeholder="From Place: House No., Sector/Colony"
+          />
+          <FormErrorMessage>{errors["Name"]}</FormErrorMessage>
+        </FormControl>
+        <FormControl w="20rem">
+          <Select
+            placeholder="To State"
+            onChange={(e) => handleOneSelect(e.target.value, "State", "To")}
           >
             {states.map((each, idx) => (
               <option value={each.value} key={idx}>
@@ -89,16 +152,25 @@ const Newtrip = () => {
         </FormControl>
         <FormControl w="20rem">
           <Select
-            placeholder="From City"
-            name="From.City"
-            onChange={handleSwitch}
+            placeholder="To City"
+            onChange={(e) => handleOneSelect(e.target.value, "City", "To")}
           >
-            {cities.map((each, idx) => (
+            {ToCities.map((each, idx) => (
               <option value={each.value} key={idx}>
                 {each.label}
               </option>
             ))}
           </Select>
+        </FormControl>
+        <FormControl isInvalid={errors["To Place"]} w="20rem">
+          <Input
+            name="To Place"
+            value={payload.To.Place}
+            onChange={(e) => handleDeepChange(e.target.value, "Place", "To")}
+            type="text"
+            placeholder="To Place: House No., Sector/Colony"
+          />
+          <FormErrorMessage>{errors["Name"]}</FormErrorMessage>
         </FormControl>
       </div>
     </>
