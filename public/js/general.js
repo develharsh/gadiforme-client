385ba3b0f7e893f121b32643f9e9cb065ebcbf4b
Harshvardhan Singh <hvsreal.223@gmail.com>
2022-08-10 22:31:24 +0530

    changed base url

--- public/js/general.js
+++ public/js/general.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:3000";
-// const BASE_URL = "https://gadifor.me";
+// const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://gadifor.me";
 
 $(document).ready(function () {
  
